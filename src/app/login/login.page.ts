f5636e98f907bcc55ece784c9c43f5d7014ca668
jcbarriar <jc.barriar@outlook.com>
2024-08-15 19:02:43 -0400

    validaciones y modificaciones en login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
     selector: 'app-login',
@@ -11,49 +13,70 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
 
     loginForm!: FormGroup;
-    data: any;
+    app_version: string;
 
     constructor(
         public formBuilder: FormBuilder,
         private apiService: ApiService,
-        private router: Router
-    ) { }
+        private router: Router,
+        private loadingCtrl: LoadingController,
+        private alertCtrl: AlertController
+    ) {
+        // la version tengo que definirla dentro del apiservice?? o debe ser en otro lado?
+        this.app_version = this.apiService.app_version;
+    }
 
     ngOnInit() {
         console.log('Inicio Login');
         this.loginForm = this.formBuilder.group({
-            email: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
             password: ['', Validators.required]
         });
 
+        console.log(this.apiService.app_version);
         console.log(this.loginForm);
     }
 
     login() {
         console.log('logear')
+
         if (this.loginForm.valid) {
             console.log(this.loginForm.value);
             this.loadData();
         } else {
+            console.log(this.loginForm);
             return console.log('faltan campos');
         }
     }
 
-    loadData() {
-        this.apiService.post('login', this.loginForm.value).subscribe(
-            response => {
-                this.data = response;
-                console.log(this.data);
+    async loadData() {
+        const loading = await this.loadingCtrl.create({
+            message: 'Validando credenciales...'
+        });
+        await loading.present();
 
-                if (this.data.status === 200 && this.data.usuario) {
+        this.apiService.post('login', this.loginForm.value).subscribe(
+            async response => {
+                await loading.dismiss();
+                if (response.status === 200 && response.usuario) {
                     // Redirige a la página de inicio
                     this.router.navigate(['/home']);
                 }
             },
-            error => {
+            async error => {
+                await loading.dismiss();
+                const alert = await this.alertCtrl.create({
+                    header: 'Error',
+                    subHeader: error.error.message,
+                    message: error.error.status,
+                    buttons: ['OK']
+                });
+
+                await alert.present();
                 console.error('Error posting data', error);
             }
         );
     }
+
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,31 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  private apiUrl = 'http://localhost:8000/api';
+    private apiUrl = 'http://localhost:8000/api';
+    app_version: string = '0.0.1';
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  // Método para obtener datos de la API
-  get(endpoint: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${endpoint}`);
-  }
+    get(endpoint: string): Observable<any> {
+        return this.http.get(`${this.apiUrl}/${endpoint}`);
+    }
 
-  // Método para enviar datos a la API
-  post(endpoint: string, data: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/${endpoint}`, data);
-  }
+    post(endpoint: string, data: any): Observable<any> {
+        return this.http.post(`${this.apiUrl}/${endpoint}`, data);
+    }
 
-  // Método para actualizar datos en la API
-  put(endpoint: string, data: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${endpoint}`, data);
-  }
+    put(endpoint: string, data: any): Observable<any> {
+        return this.http.put(`${this.apiUrl}/${endpoint}`, data);
+    }
 
-  // Método para eliminar datos en la API
-  delete(endpoint: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${endpoint}`);
-  }
+    delete(endpoint: string): Observable<any> {
+        return this.http.delete(`${this.apiUrl}/${endpoint}`);
+    }
 }
